41f1717e2225dfd9892cfb113740ad8ca9940b27
JOE_SURFACE\School <jeanellg@uci.edu>
2017-03-15 12:47:53 -0700

    added main menu

---
+++ final_game/Assets/scripts/main_menu_.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class main_menu_ : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Start_button()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
