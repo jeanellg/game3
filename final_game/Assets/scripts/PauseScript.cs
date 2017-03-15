0b96c680002de40efd3581c5992d155097adcec2
JOE_SURFACE\School <jeanellg@uci.edu>
2017-03-15 14:07:09 -0700

    added     main menu (will finish pic later)\     instructions     pause menu prefab

---
+++ final_game/Assets/scripts/PauseScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+
+    GameObject pauseMenu;
+    bool paused;
+    // Use this for initialization
+    void Start()
+    {
+        paused = false;
+        pauseMenu = GameObject.Find("pauseMenu");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            paused = !paused;
+        }
+        if (paused)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if (!paused)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+    public void Resume()
+    {
+        paused = false;
+    }
+
+    public void back_to_main()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
